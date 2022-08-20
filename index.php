a4907877341ae2a75d80b3c544cb9ab0c512f6b9
PaterDeTenebis <onleerave@gmail.com>
2022-08-20 18:23:08 +0300

    top&bot template done

--- index.php
+++ index.php
@@ -2,16 +2,30 @@
 if ($_SERVER['REQUEST_URI'] == '/') $page = 'home';
 else {
     $page = substr($_SERVER['REQUEST_URI'], 1);
-    if(!preg_match('/^[A-z0-9]{3-15}$/', $page)) exit('url error');
+    if (!preg_match('/^[A-z0-9]{3-15}$/', $page)) exit('url error');
 }
 
 session_start();
 
-if(file_exists('all/' .$page. '.php')) include 'all/' .$page. '.php';
-else if($_SESSION['ulogin'] != 1 and file_exists('guest/' .$page. '.php')) include 'guest' .$page. '.php';
-else if($_SESSION['ulogin'] == 1 and file_exists('auth/' .$page. '.php')) include 'auth/' .$page. '.php';
+if (file_exists('all/' . $page . '.php')) include 'all/' . $page . '.php';
+else if ($_SESSION['ulogin'] != 1 and file_exists('guest/' . $page . '.php')) include 'guest' . $page . '.php';
+else if ($_SESSION['ulogin'] == 1 and file_exists('auth/' . $page . '.php')) include 'auth/' . $page . '.php';
 else exit('Page not found');
 
+function top($title)
+{
+    echo '<!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>' . $title . '</title>
+                <link rel="stylesheet" href="/css/style.css">
+            </head>
+            <body>';
+}
 
-
-?>
+function bottom()
+{
+    echo '</body>
+            <html>';
+}
